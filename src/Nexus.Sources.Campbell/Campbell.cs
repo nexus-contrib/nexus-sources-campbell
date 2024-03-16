92df75f2ee7b0c62dc584d8ae9cf6aa667eb7c40
Apollo3zehn <git@m1.apollo3zehn.net>
2024-03-16 22:28:12 +0100

    Follow Nexus changes

--- src/Nexus.Sources.Campbell/Campbell.cs
+++ src/Nexus.Sources.Campbell/Campbell.cs
@@ -6,196 +6,186 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-namespace Nexus.Sources
+namespace Nexus.Sources;
+
+[ExtensionDescription(
+    "Provides access to databases with Campbell DAT files.",
+    "https://github.com/Apollo3zehn/nexus-sources-campbell",
+    "https://github.com/Apollo3zehn/nexus-sources-campbell")]
+public class Campbell : StructuredFileDataSource
 {
-    [ExtensionDescription(
-        "Provides access to databases with Campbell DAT files.", 
-        "https://github.com/Apollo3zehn/nexus-sources-campbell", 
-        "https://github.com/Apollo3zehn/nexus-sources-campbell")]
-    public class Campbell : StructuredFileDataSource
+    record CatalogDescription(
+        string Title,
+        Dictionary<string, IReadOnlyList<FileSource>> FileSourceGroups,
+        JsonElement? AdditionalProperties);
+
+    private Dictionary<string, CatalogDescription> _config = default!;
+
+    protected override async Task InitializeAsync(CancellationToken cancellationToken)
     {
-        record CatalogDescription(
-            string Title,
-            Dictionary<string, IReadOnlyList<FileSource>> FileSourceGroups, 
-            JsonElement? AdditionalProperties);
+        var configFilePath = Path.Combine(Root, "config.json");
 
-        #region Fields
+        if (!File.Exists(configFilePath))
+            throw new Exception($"Configuration file {configFilePath} not found.");
 
-        private Dictionary<string, CatalogDescription> _config = default!;
+        var jsonString = await File.ReadAllTextAsync(configFilePath, cancellationToken);
+        _config = JsonSerializer.Deserialize<Dictionary<string, CatalogDescription>>(jsonString) ?? throw new Exception("config is null");
+    }
 
-        #endregion
+    protected override Task<Func<string, Dictionary<string, IReadOnlyList<FileSource>>>> GetFileSourceProviderAsync(
+        CancellationToken cancellationToken)
+    {
+        return Task.FromResult<Func<string, Dictionary<string, IReadOnlyList<FileSource>>>>(
+            catalogId => _config[catalogId].FileSourceGroups);
+    }
 
-        #region Methods
+    protected override Task<CatalogRegistration[]> GetCatalogRegistrationsAsync(string path, CancellationToken cancellationToken)
+    {
+        if (path == "/")
+            return Task.FromResult(_config.Select(entry => new CatalogRegistration(entry.Key, entry.Value.Title)).ToArray());
 
-        protected override async Task InitializeAsync(CancellationToken cancellationToken)
+        else
+            return Task.FromResult(Array.Empty<CatalogRegistration>());
+    }
+
+    protected override Task<ResourceCatalog> GetCatalogAsync(string catalogId, CancellationToken cancellationToken)
+    {
+        var catalogDescription = _config[catalogId];
+        var catalog = new ResourceCatalog(id: catalogId);
+
+        foreach (var (fileSourceId, fileSourceGroup) in catalogDescription.FileSourceGroups)
         {
-            var configFilePath = Path.Combine(Root, "config.json");
-
-            if (!File.Exists(configFilePath))
-                throw new Exception($"Configuration file {configFilePath} not found.");
-
-            var jsonString = await File.ReadAllTextAsync(configFilePath, cancellationToken);
-            _config = JsonSerializer.Deserialize<Dictionary<string, CatalogDescription>>(jsonString) ?? throw new Exception("config is null");
-        }
-
-        protected override Task<Func<string, Dictionary<string, IReadOnlyList<FileSource>>>> GetFileSourceProviderAsync(
-            CancellationToken cancellationToken)
-        {
-            return Task.FromResult<Func<string, Dictionary<string, IReadOnlyList<FileSource>>>>(
-                catalogId => _config[catalogId].FileSourceGroups);
-        }
-
-        protected override Task<CatalogRegistration[]> GetCatalogRegistrationsAsync(string path, CancellationToken cancellationToken)
-        {
-            if (path == "/")
-                return Task.FromResult(_config.Select(entry => new CatalogRegistration(entry.Key, entry.Value.Title)).ToArray());
-
-            else
-                return Task.FromResult(Array.Empty<CatalogRegistration>());
-        }
-
-        protected override Task<ResourceCatalog> GetCatalogAsync(string catalogId, CancellationToken cancellationToken)
-        {
-            var catalogDescription = _config[catalogId];
-            var catalog = new ResourceCatalog(id: catalogId);
-
-            foreach (var (fileSourceId, fileSourceGroup) in catalogDescription.FileSourceGroups)
+            foreach (var fileSource in fileSourceGroup)
             {
-                foreach (var fileSource in fileSourceGroup)
+                var filePaths = default(string[]);
+
+                var catalogSourceFiles = fileSource.AdditionalProperties?.GetStringArray("CatalogSourceFiles");
+
+                if (catalogSourceFiles is not null)
                 {
-                    var filePaths = default(string[]);
+                    filePaths = catalogSourceFiles
+                        .Where(filePath => filePath is not null)
+                        .Select(filePath => Path.Combine(Root, filePath!))
+                        .ToArray();
+                }
+                else
+                {
+                    if (!TryGetFirstFile(fileSource, out var filePath))
+                        continue;
 
-                    var catalogSourceFiles = fileSource.AdditionalProperties?.GetStringArray("CatalogSourceFiles");
+                    filePaths = [filePath];
+                }
 
-                    if (catalogSourceFiles is not null)
+                cancellationToken.ThrowIfCancellationRequested();
+
+                foreach (var filePath in filePaths)
+                {
+                    var newCatalogBuilder = new ResourceCatalogBuilder(id: catalogId);
+
+                    using var campbellFile = new CampbellFile(filePath);
+
+                    foreach (var campbellVariable in campbellFile.Variables)
                     {
-                        filePaths = catalogSourceFiles
-                            .Where(filePath => filePath is not null)
-                            .Select(filePath => Path.Combine(Root, filePath!))
-                            .ToArray();
-                    }
-                    else
-                    {
-                        if (!TryGetFirstFile(fileSource, out var filePath))
-                            continue;
+                        cancellationToken.ThrowIfCancellationRequested();
 
-                        filePaths = new[] { filePath };
-                    }
-
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    foreach (var filePath in filePaths)
-                    {
-                        var newCatalogBuilder = new ResourceCatalogBuilder(id: catalogId);
-
-                        using var campbellFile = new CampbellFile(filePath);
-
-                        foreach (var campbellVariable in campbellFile.Variables)
+                        try
                         {
-                            cancellationToken.ThrowIfCancellationRequested();
+                            var additionalProperties = fileSource.AdditionalProperties;
+                            var samplePeriodString = (additionalProperties?.GetStringValue("SamplePeriod")) ?? throw new Exception("The configuration parameter SamplePeriod is required.");
+                            var samplePeriod = TimeSpan.Parse(samplePeriodString);
 
-                            try
-                            {
-                                var additionalProperties = fileSource.AdditionalProperties;
-                                var samplePeriodString = additionalProperties?.GetStringValue("SamplePeriod");
+                            var representation = new Representation(
+                                dataType: Utilities.GetNexusDataTypeFromType(campbellVariable.DataType),
+                                samplePeriod: samplePeriod);
 
-                                if (samplePeriodString is null)
-                                    throw new Exception("The configuration parameter SamplePeriod is required.");
+                            if (!TryEnforceNamingConvention(campbellVariable.Name, additionalProperties, out var resourceId))
+                                continue;
 
-                                var samplePeriod = TimeSpan.Parse(samplePeriodString);
+                            var resource = new ResourceBuilder(id: resourceId)
+                                .WithUnit(campbellVariable.Unit)
+                                .WithGroups(fileSourceId)
+                                .WithFileSourceId(fileSourceId)
+                                .WithOriginalName(campbellVariable.Name)
+                                .AddRepresentation(representation)
+                                .Build();
 
-                                var representation = new Representation(
-                                    dataType: Utilities.GetNexusDataTypeFromType(campbellVariable.DataType),
-                                    samplePeriod: samplePeriod);
-
-                                if (!TryEnforceNamingConvention(campbellVariable.Name, additionalProperties, out var resourceId))
-                                    continue;
-
-                                var resource = new ResourceBuilder(id: resourceId)
-                                    .WithUnit(campbellVariable.Unit)
-                                    .WithGroups(fileSourceId)
-                                    .WithFileSourceId(fileSourceId)
-                                    .WithOriginalName(campbellVariable.Name)
-                                    .AddRepresentation(representation)
-                                    .Build();
-
-                                newCatalogBuilder.AddResource(resource);
-                            }
-                            catch (NotSupportedException) // for invalid data type
-                            {
-                                // skip
-                            }
+                            newCatalogBuilder.AddResource(resource);
                         }
-
-                        catalog = catalog.Merge(newCatalogBuilder.Build());
+                        catch (NotSupportedException) // for invalid data type
+                        {
+                            // skip
+                        }
                     }
+
+                    catalog = catalog.Merge(newCatalogBuilder.Build());
                 }
             }
-
-            return Task.FromResult(catalog);
         }
 
-        protected override Task ReadAsync(ReadInfo info, StructuredFileReadRequest[] readRequests, CancellationToken cancellationToken)
+        return Task.FromResult(catalog);
+    }
+
+    protected override Task ReadAsync(
+        ReadInfo info, 
+        StructuredFileReadRequest[] readRequests, 
+        CancellationToken cancellationToken)
+    {
+        return Task.Run(async () =>
         {
-            return Task.Run(async () =>
+            foreach (var readRequest in readRequests)
             {
-                foreach (var readRequest in readRequests)
+                using var campbellFile = new CampbellFile(info.FilePath);
+                var fileSourceProvider = await GetFileSourceProviderAsync(cancellationToken);
+
+                var campbellVariable = campbellFile.Variables.First(current => current.Name == readRequest.OriginalName);
+                var (timeStamps, data) = campbellFile.Read<byte>(campbellVariable);
+                var result = data.Buffer;
+                var elementSize = readRequest.CatalogItem.Representation.ElementSize;
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                // write data
+                if (result.Length == info.FileLength * elementSize)
                 {
-                    using var campbellFile = new CampbellFile(info.FilePath);
-                    var fileSourceProvider = await GetFileSourceProviderAsync(cancellationToken);
+                    var offset = (int)info.FileOffset * elementSize;
+                    var length = (int)info.FileBlock * elementSize;
 
-                    var campbellVariable = campbellFile.Variables.First(current => current.Name == readRequest.OriginalName);
-                    var (timeStamps, data) = campbellFile.Read<byte>(campbellVariable);
-                    var result = data.Buffer;
-                    var elementSize = readRequest.CatalogItem.Representation.ElementSize;
+                    result
+                        .AsMemory()
+                        .Slice(offset, length)
+                        .CopyTo(readRequest.Data);
 
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    // write data
-                    if (result.Length == info.FileLength * elementSize)
-                    {
-                        var offset = (int)info.FileOffset * elementSize;
-                        var length = (int)info.FileBlock * elementSize;
-
-                        result
-                            .AsMemory()
-                            .Slice(offset, length)
-                            .CopyTo(readRequest.Data);
-
-                        readRequest
-                            .Status
-                            .Span
-                            .Fill(1);
-                    }
-                    // skip data
-                    else
-                    {
-                        Logger.LogDebug("The actual buffer size does not match the expected size, which indicates an incomplete file");
-                    }
+                    readRequest
+                        .Status
+                        .Span
+                        .Fill(1);
                 }
-            }, cancellationToken);
-        }
+                // skip data
+                else
+                {
+                    Logger.LogDebug("The actual buffer size does not match the expected size, which indicates an incomplete file");
+                }
+            }
+        }, cancellationToken);
+    }
 
-        private static bool TryEnforceNamingConvention(
-            string resourceId, 
-            JsonElement? additionalProperties, 
-            [NotNullWhen(returnValue: true)] out string newResourceId)
-        {
-            var replacePattern = additionalProperties?.GetStringValue("ReplacePattern");
-            var replaceValue = additionalProperties?.GetStringValue("ReplaceValue");
+    private static bool TryEnforceNamingConvention(
+        string resourceId,
+        JsonElement? additionalProperties,
+        [NotNullWhen(returnValue: true)] out string newResourceId)
+    {
+        var replacePattern = additionalProperties?.GetStringValue("ReplacePattern");
+        var replaceValue = additionalProperties?.GetStringValue("ReplaceValue");
 
-            if (replacePattern is null || replaceValue is null)
-                newResourceId = resourceId;
+        if (replacePattern is null || replaceValue is null)
+            newResourceId = resourceId;
 
-            else
-                newResourceId = Regex.Replace(resourceId, replacePattern, replaceValue);
+        else
+            newResourceId = Regex.Replace(resourceId, replacePattern, replaceValue);
 
-            newResourceId = Resource.InvalidIdCharsExpression.Replace(newResourceId, "");
-            newResourceId = Resource.InvalidIdStartCharsExpression.Replace(newResourceId, "");
+        newResourceId = Resource.InvalidIdCharsExpression.Replace(newResourceId, "");
+        newResourceId = Resource.InvalidIdStartCharsExpression.Replace(newResourceId, "");
 
-            return Resource.ValidIdExpression.IsMatch(newResourceId);
-        }
-
-        #endregion
+        return Resource.ValidIdExpression.IsMatch(newResourceId);
     }
 }
--- tests/Nexus.Sources.Campbell.Tests/CampbellTests.cs
+++ tests/Nexus.Sources.Campbell.Tests/CampbellTests.cs
@@ -4,128 +4,127 @@ using Nexus.Extensibility;
 using System.Runtime.InteropServices;
 using Xunit;
 
-namespace Nexus.Sources.Tests
+namespace Nexus.Sources.Tests;
+
+public class CampbellTests
 {
-    public class CampbellTests
+    [Fact]
+    public async Task ProvidesCatalog()
     {
-        [Fact]
-        public async Task ProvidesCatalog()
+        // arrange
+        var dataSource = new Campbell() as IDataSource;
+
+        var context = new DataSourceContext(
+            ResourceLocator: new Uri("Database", UriKind.Relative),
+            SystemConfiguration: default!,
+            SourceConfiguration: default!,
+            RequestConfiguration: default!);
+
+        await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+        // act
+        var actual = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
+        var actualIds = actual.Resources!.Skip(9).Take(2).Select(resource => resource.Id).ToList();
+        var actualUnits = actual.Resources!.Skip(9).Take(2).Select(resource => resource.Properties?.GetStringValue("unit")).ToList();
+        var actualGroups = actual.Resources!.Skip(9).Take(2).SelectMany(resource => resource.Properties?.GetStringArray("groups")!).ToList();
+        var (begin, end) = await dataSource.GetTimeRangeAsync("/A/B/C", CancellationToken.None);
+
+        // assert
+        var expectedIds = new List<string>() { "aaaa_55_SonicTempC", "bbbbb_25_Vx" };
+        var expectedUnits = new List<string>() { "", "" };
+        var expectedGroups = new List<string>() { "aaaa", "bbbbb" };
+        var expectedStartDate = new DateTime(2015, 10, 05, 12, 00, 00, DateTimeKind.Utc);
+        var expectedEndDate = new DateTime(2015, 10, 05, 14, 00, 00, DateTimeKind.Utc);
+
+        Assert.True(expectedIds.SequenceEqual(actualIds));
+        Assert.True(expectedUnits.SequenceEqual(actualUnits));
+        Assert.True(expectedGroups.SequenceEqual(actualGroups));
+        Assert.Equal(expectedStartDate, begin);
+        Assert.Equal(expectedEndDate, end);
+    }
+
+    [Fact]
+    public async Task ProvidesDataAvailability()
+    {
+        // arrange
+        var dataSource = new Campbell() as IDataSource;
+
+        var context = new DataSourceContext(
+            ResourceLocator: new Uri("Database", UriKind.Relative),
+            SystemConfiguration: default!,
+            SourceConfiguration: default!,
+            RequestConfiguration: default!);
+
+        await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+        // act
+        var actual = new Dictionary<DateTime, double>();
+        var begin = new DateTime(2015, 10, 04, 0, 0, 0, DateTimeKind.Utc);
+        var end = new DateTime(2015, 10, 06, 0, 0, 0, DateTimeKind.Utc);
+
+        var currentBegin = begin;
+
+        while (currentBegin < end)
         {
-            // arrange
-            var dataSource = new Campbell() as IDataSource;
-
-            var context = new DataSourceContext(
-                ResourceLocator: new Uri("Database", UriKind.Relative),
-                SystemConfiguration: default!,
-                SourceConfiguration: default!,
-                RequestConfiguration: default!);
-
-            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
-
-            // act
-            var actual = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
-            var actualIds = actual.Resources!.Skip(9).Take(2).Select(resource => resource.Id).ToList();
-            var actualUnits = actual.Resources!.Skip(9).Take(2).Select(resource => resource.Properties?.GetStringValue("unit")).ToList();
-            var actualGroups =  actual.Resources!.Skip(9).Take(2).SelectMany(resource => resource.Properties?.GetStringArray("groups")!).ToList();
-            var (begin, end) = await dataSource.GetTimeRangeAsync("/A/B/C", CancellationToken.None);
-
-            // assert
-            var expectedIds = new List<string>() { "aaaa_55_SonicTempC", "bbbbb_25_Vx" };
-            var expectedUnits = new List<string>() { "", "" };
-            var expectedGroups = new List<string>() { "aaaa", "bbbbb" };
-            var expectedStartDate = new DateTime(2015, 10, 05, 12, 00, 00, DateTimeKind.Utc);
-            var expectedEndDate = new DateTime(2015, 10, 05, 14, 00, 00, DateTimeKind.Utc);
-
-            Assert.True(expectedIds.SequenceEqual(actualIds));
-            Assert.True(expectedUnits.SequenceEqual(actualUnits));
-            Assert.True(expectedGroups.SequenceEqual(actualGroups));
-            Assert.Equal(expectedStartDate, begin);
-            Assert.Equal(expectedEndDate, end);
+            actual[currentBegin] = await dataSource.GetAvailabilityAsync("/A/B/C", currentBegin, currentBegin.AddDays(1), CancellationToken.None);
+            currentBegin += TimeSpan.FromDays(1);
         }
 
-        [Fact]
-        public async Task ProvidesDataAvailability()
+        // assert
+        var expected = new SortedDictionary<DateTime, double>(Enumerable.Range(0, 2).ToDictionary(
+                i => begin.AddDays(i),
+                i => 0.0))
         {
-            // arrange
-            var dataSource = new Campbell() as IDataSource;
+            [begin.AddDays(0)] = 0,
+            [begin.AddDays(1)] = 4 / 48.0
+        };
 
-            var context = new DataSourceContext(
-                ResourceLocator: new Uri("Database", UriKind.Relative),
-                SystemConfiguration: default!,
-                SourceConfiguration: default!,
-                RequestConfiguration: default!);
+        Assert.True(expected.SequenceEqual(new SortedDictionary<DateTime, double>(actual)));
+    }
 
-            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+    [Fact]
+    public async Task CanReadFullDay()
+    {
+        // arrange
+        var dataSource = new Campbell() as IDataSource;
 
-            // act
-            var actual = new Dictionary<DateTime, double>();
-            var begin = new DateTime(2015, 10, 04, 0, 0, 0, DateTimeKind.Utc);
-            var end = new DateTime(2015, 10, 06, 0, 0, 0, DateTimeKind.Utc);
+        var context = new DataSourceContext(
+            ResourceLocator: new Uri("Database", UriKind.Relative),
+            SystemConfiguration: default!,
+            SourceConfiguration: default!,
+            RequestConfiguration: default!);
 
-            var currentBegin = begin;
+        await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
 
-            while (currentBegin < end)
-            {
-                actual[currentBegin] = await dataSource.GetAvailabilityAsync("/A/B/C", currentBegin, currentBegin.AddDays(1), CancellationToken.None);
-                currentBegin += TimeSpan.FromDays(1);
-            }
+        // act
+        var catalog = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
+        var resource = catalog.Resources![0];
+        var representation = resource.Representations![0];
+        var catalogItem = new CatalogItem(catalog, resource, representation, default);
 
-            // assert
-            var expected = new SortedDictionary<DateTime, double>(Enumerable.Range(0, 2).ToDictionary(
-                    i => begin.AddDays(i),
-                    i => 0.0))
-            {
-                [begin.AddDays(0)] = 0,
-                [begin.AddDays(1)] = 4 / 48.0
-            };
+        var begin = new DateTime(2015, 10, 05, 0, 0, 0, DateTimeKind.Utc);
+        var end = new DateTime(2015, 10, 06, 0, 0, 0, DateTimeKind.Utc);
+        var (data, status) = ExtensibilityUtilities.CreateBuffers(representation, begin, end);
 
-            Assert.True(expected.SequenceEqual(new SortedDictionary<DateTime, double>(actual)));
+        var result = new ReadRequest(catalogItem, data, status);
+        await dataSource.ReadAsync(begin, end, [result], default!, new Progress<double>(), CancellationToken.None);
+
+        // assert
+        void DoAssert()
+        {
+            var data = MemoryMarshal.Cast<byte, float>(result.Data.Span);
+
+            Assert.Equal(0, data[864000 - 1]);
+            Assert.Equal(8.590, data[864000 + 0], precision: 3);
+            Assert.Equal(6.506, data[1008000 - 1], precision: 3);
+            Assert.Equal(0, data[1008000 + 0]);
+
+            Assert.Equal(0, result.Status.Span[864000 - 1]);
+            Assert.Equal(1, result.Status.Span[864000 + 0]);
+            Assert.Equal(1, result.Status.Span[1008000 - 1]);
+            Assert.Equal(0, result.Status.Span[1008000 + 0]);
         }
 
-        [Fact]
-        public async Task CanReadFullDay()
-        {
-            // arrange
-            var dataSource = new Campbell() as IDataSource;
-
-            var context = new DataSourceContext(
-                ResourceLocator: new Uri("Database", UriKind.Relative),
-                SystemConfiguration: default!,
-                SourceConfiguration: default!,
-                RequestConfiguration: default!);
-
-            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
-
-            // act
-            var catalog = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
-            var resource = catalog.Resources![0];
-            var representation = resource.Representations![0];
-            var catalogItem = new CatalogItem(catalog, resource, representation, default);
-
-            var begin = new DateTime(2015, 10, 05, 0, 0, 0, DateTimeKind.Utc);
-            var end = new DateTime(2015, 10, 06, 0, 0, 0, DateTimeKind.Utc);
-            var (data, status) = ExtensibilityUtilities.CreateBuffers(representation, begin, end);
-
-            var result = new ReadRequest(catalogItem, data, status);
-            await dataSource.ReadAsync(begin, end, new ReadRequest[] { result }, default!, new Progress<double>(), CancellationToken.None);
-
-            // assert
-            void DoAssert()
-            {
-                var data = MemoryMarshal.Cast<byte, float>(result.Data.Span);
-
-                Assert.Equal(0, data[864000 - 1]);
-                Assert.Equal(8.590, data[864000 + 0], precision: 3);
-                Assert.Equal(6.506, data[1008000 - 1], precision: 3);
-                Assert.Equal(0, data[1008000 + 0]);
-
-                Assert.Equal(0, result.Status.Span[864000 - 1]);
-                Assert.Equal(1, result.Status.Span[864000 + 0]);
-                Assert.Equal(1, result.Status.Span[1008000 - 1]);
-                Assert.Equal(0, result.Status.Span[1008000 + 0]);
-            }
-
-            DoAssert();
-        }
+        DoAssert();
     }
 }
